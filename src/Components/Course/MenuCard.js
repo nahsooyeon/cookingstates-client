91516d87cb4e907b9ec2ad7916d2bda52f5bc171
nahsooyeon <nahsooyeon@gmail.com>
2021-03-21 14:32:52 +0900

    💄스타일:Menucard CSS 폰트 컬러 수정

--- src/Components/Course/MenuCard.js
+++ src/Components/Course/MenuCard.js
@@ -13,7 +13,7 @@ function MenuCard(props) {
           alt="course1"
         ></img>
         <div className="menu-name">{recipe[1].title}</div>
-        <div className="difficulty">{"⭑".repeat(recipe[1].difficulty)}</div>
+        <div className="difficulty-1">{"⭑".repeat(recipe[1].difficulty)}</div>
       </div>
       <div className="menu-card">
         <img
@@ -22,7 +22,7 @@ function MenuCard(props) {
           alt="course1"
         ></img>
         <div className="menu-name">{recipe[0].title}</div>
-        <div className="difficulty">{"⭑".repeat(recipe[0].difficulty)}</div>
+        <div className="difficulty-2">{"⭑".repeat(recipe[0].difficulty)}</div>
       </div>
       <div className="menu-card">
         <img
@@ -31,7 +31,7 @@ function MenuCard(props) {
           alt="course1"
         ></img>
         <div className="menu-name">{recipe[2].title}</div>
-        <div className="difficulty">{"⭑".repeat(recipe[2].difficulty)}</div>
+        <div className="difficulty-3">{"⭑".repeat(recipe[2].difficulty)}</div>
       </div>
       <div className="menu-card">
         <img
@@ -40,7 +40,7 @@ function MenuCard(props) {
           alt="course1"
         ></img>
         <div className="menu-name">{recipe[3].title}</div>
-        <div className="difficulty">{"⭑".repeat(recipe[3].difficulty)}</div>
+        <div className="difficulty-4">{"⭑".repeat(recipe[3].difficulty)}</div>
       </div>
       <div className="menu-card">
         <img
@@ -49,7 +49,7 @@ function MenuCard(props) {
           alt="course1"
         ></img>
         <div className="menu-name">{recipe[4].title}</div>
-        <div className="difficulty">{"⭑".repeat(recipe[4].difficulty)}</div>
+        <div className="difficulty-5">{"⭑".repeat(recipe[4].difficulty)}</div>
       </div>
     </>
   );
